35c9f82ebfe657d59efec6fda34f03f033b0c591
kacperwnuk <kacperwnuk97@o2.pl>
2019-11-11 20:35:25 +0100

    Testing migrating postgresql db

--- Recipes/models.py
+++ Recipes/models.py
@@ -1,6 +1,8 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=1000)
+    rate = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], default=3)
