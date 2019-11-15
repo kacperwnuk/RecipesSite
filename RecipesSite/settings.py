33fd5c59bc054f1958d895c4437054e3af9a13b5
kacperwnuk <kacperwnuk97@o2.pl>
2019-11-15 20:20:45 +0100

    Fix to make static files working when debug set to false

--- RecipesSite/settings.py
+++ RecipesSite/settings.py
@@ -133,6 +133,6 @@ USE_TZ = True
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'RecipesSite/static')]
 
